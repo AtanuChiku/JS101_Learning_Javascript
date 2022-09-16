5f810620a137889aa9d5dc3c14acbeb5ca15da16
Atanu Mandal <atanuchiku@gmail.com>
2022-09-16 17:26:35 +0530

    variables problem

---
+++ Variable/problem1.js
@@ -0,0 +1,6 @@
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+let a = "Masai School";
+let b = "Transformation in Education";
+
+console.log(a + "\n" + b);
---
+++ Variable/problem4.js
@@ -0,0 +1,35 @@
+let ReportCard="|| REPORT CARD ||";
+let design1="--------------------------";
+
+let NI="Name of the Institute: Nimpith Ramkrishna Vidyabhavan";
+
+let design2="--------------------------------------------------------";
+
+let name ="* Student Name: Atanu Mandal";
+let Class="* Class: 10";
+let Sc ="* Section: 'A'";
+let design3="--------------------------";
+
+let Grade ="Grade: 70%";
+
+let design4="--------------------------";
+let subject1="> English : 76";
+let subject2="> Mathematics : 86";
+let subject3="> Physical Science : 72";
+
+
+
+console.log(ReportCard);
+console.log(design1);
+console.log(NI);
+console.log(design2);
+console.log(name);
+console.log(Class);
+console.log(Sc);
+console.log(design3);
+console.log(Grade);
+console.log(design4);
+console.log(subject1);
+console.log(subject2);
+console.log(subject3);
+
