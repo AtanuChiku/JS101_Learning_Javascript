e6387dd347de11da30edaece5281c2ccdfd4b7f3
Atanu Mandal <atanuchiku@gmail.com>
2022-09-16 17:35:35 +0530

    Mathematics Operators

---
+++ Mathematics/problem3.js
@@ -0,0 +1,15 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+
+let dubtwo = 2*2;
+let dubfive = 5*2;
+let dubsix = 6*2;
+
+let trithree = 3*3;
+let trifour = 4*3;
+
+console.log(one+dubtwo+dubfive+dubsix+trithree+trifour);
